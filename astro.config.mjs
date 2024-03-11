6946f3d89f3ac1d3e2e81593bb72a1d5832a95c9
lars <17064080+liarbeast@users.noreply.github.com>
2024-03-11 21:41:41 +0100

    Try adding base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
@@ -8,3 +9,13 @@ export default defineConfig({
 	integrations: [tailwind({}), mdx()],
 	site: 'https://liarbeast.github.io'
 })
+=======
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind({})]
+});
+>>>>>>> parent of 9f1e9b4 (Before add cloudflare)
